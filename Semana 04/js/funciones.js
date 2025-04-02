40329bda1139f49b47f4de7e07753636d085f738
marioaje <marioaje@gmail.com>
2025-04-02 13:27:15 -0600

    Parte 1

---
+++ Semana 04/js/funciones.js
@@ -0,0 +1,7 @@
+export function saludo ( nombre ){
+        return "Hola " + nombre;
+}
+
+export function sumatoria (){
+        return 3+ 6;
+}
---
+++ Semana 04/js/usandofunciones.js
@@ -0,0 +1,5 @@
+import { saludo, sumatoria } from './funciones.js';
+
+
+
+document.getElementById("parrafoTextp").textContent = saludo("Mario");
