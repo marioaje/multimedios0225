2c4131a8595e78b40444029aafa8797b45f3554c
marioaje <marioaje@gmail.com>
2025-04-04 10:42:56 -0600

    json objeto

---
+++ Semana 04/js/objetoJson.js
@@ -0,0 +1,32 @@
+let instrumento = {
+    clase: "Cuerda",
+    accesorrios : "tiene",
+    nombre : "tambor",
+    precio : 30000
+}
+
+let jsoninstrumento = JSON.stringify(instrumento);
+
+
+function cargarDatos(){
+    console.log(instrumento);
+    console.log(jsoninstrumento);
+
+    let jsoninstrumentoObjeto = JSON.parse(jsoninstrumento);
+
+    let estructuraweb =     `<ul>
+                                <li>${jsoninstrumentoObjeto.clase}</li>
+                                <li>${jsoninstrumentoObjeto.accesorrios}</li>
+                                <li>${jsoninstrumentoObjeto.nombre}</li>
+                                <li>${jsoninstrumentoObjeto.precio}</li>
+                            </ul>` ;
+
+
+    document.getElementById("clase").value = jsoninstrumentoObjeto.clase;
+    document.getElementById("accesorrios").value = jsoninstrumentoObjeto.accesorrios;
+    document.getElementById("nombre").value = jsoninstrumentoObjeto.nombre;
+    document.getElementById("precio").value = jsoninstrumentoObjeto.precio;
+
+
+    document.getElementById("resultadodatos").innerHTML = estructuraweb;
+}
