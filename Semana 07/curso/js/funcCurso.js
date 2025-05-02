9f98b906ed8ad4adb042b97635878cf963b6037f
marioaje <marioaje@gmail.com>
2025-05-02 11:11:24 -0600

    Jquery

--- Semana 07/curso/js/funcCurso.js
+++ Semana 07/curso/js/funcCurso.js
@@ -38,57 +38,109 @@ const mensajeEliminar = `
 
 $(document).ready(function () {
     obtenerDatos();
-});
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    obtenerDatos();
-
-
-    document.getElementById('formualCrear').addEventListener('submit', (eventos) => {
-        eventos.preventDefault();
+    $("#formualCrear").submit(function (e) { 
+        e.preventDefault();
         crearCursos();
-    })
+    });
 
-    document.getElementById('formualEditar').addEventListener('submit', (eventos) => {
-        eventos.preventDefault();
+
+    $("#formualEditar").submit(function (e) { 
+        e.preventDefault();
         actualizarCursos();
-    })
-
-    
-    document.getElementById('formualEliminar').addEventListener('submit', (eventos) => {
-        eventos.preventDefault();
-        eliminarCursos();
-    })
-
-
-    
-
-
+    });
 });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     obtenerDatos();
+
+
+//     document.getElementById('formualCrear').addEventListener('submit', (eventos) => {
+//         eventos.preventDefault();
+//         crearCursos();
+//     })
+
+//     document.getElementById('formualEditar').addEventListener('submit', (eventos) => {
+//         eventos.preventDefault();
+//         actualizarCursos();
+//     })
+
+    
+//     document.getElementById('formualEliminar').addEventListener('submit', (eventos) => {
+//         eventos.preventDefault();
+//         eliminarCursos();
+//     })
+
+
+    
+
+
+// });
 
 
 function obtenerDatos() {
 
 
-    fetch(`${URLAPI}${lista}`, {
-            method: 'POST'
-        })
-        .then(response => response.json())
-        .then(
-            data => datosTabla(data.data)
-        )
-        .catch(error => console.error(error));
+    // fetch(`${URLAPI}${lista}`, {
+    //         method: 'POST'
+    //     })
+    //     .then(response => response.json())
+    //     .then(
+    //         data => datosTabla(data.data)
+    //     )
+    //     .catch(error => console.error(error));
 
         $.ajax({
-            type: "method",
-            url: "url",
-            data: "data",
-            dataType: "dataType",
-            success: function (response) {
-                
+            type: "POST",
+            url: `${URLAPI}${lista}`,            
+            dataType: "json",
+            beforeSend: function(){
+                let variableMensaje = `  <button
+                class="btn btn-warning"
+                type="button"
+                disabled
+              >
+                <span
+                    class="spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden="true"
+                ></span>
+                Loading...
+              </button>`;
+
+              $("#mensajes").html(variableMensaje);
+
+
+
             },
-            error: function(),
+            success: function (response) {
+                console.log(response);
+                datosTabla(response.data);
+                $("#mensajes").html("");
+            },
+            error: function ( xhr, status, error){
+
+                const mensajeEliminar = `
+                    <div
+                                class="alert alert-danger alert-dismissible fade show"
+                                role="alert"
+                            >
+                                <button
+                                    type="button"
+                                    class="btn-close"
+                                    data-bs-dismiss="alert"
+                                    aria-label="Close"
+                                ></button>
+                                <strong>Error: </strong> ${error} <br>
+                                <strong>status: </strong> ${status} <br>
+                                <strong>xhr: </strong> ${xhr} <br>
+                            </div>`;
+                            
+                    $("#mensajes").html(mensajeEliminar);
+                    
+            }
+            
             
         });
 
@@ -97,8 +149,11 @@ function obtenerDatos() {
 function datosTabla(data) {
    // console.log(data);
     //$(selector).html(htmlString);
-    const datostabla = document.getElementById('datostabla');
-    datostabla.innerHTML = '';
+    // const datostabla = document.getElementById('datostabla');
+    // datostabla.innerHTML = '';
+
+    const $datostabla = $("#datostabla");
+    $datostabla.empty();
 
     data.forEach(element => {
 
@@ -134,32 +189,91 @@ function datosTabla(data) {
                             </td>
                      </tr>        
                  `;
-
-        datostabla.innerHTML += fila;
+    //$(selector).html(htmlString);
+       // datostabla.innerHTML += fila;
+       $datostabla.append(fila);
     });
 
 }
 
 
 function crearCursos() {
+    alert("Creando curso");
+    // const nombre = document.getElementById('nombre').value;
+    // const descripcion = document.getElementById('descripcion').value;
+    // const tiempo = document.getElementById('tiempo').value;
 
-    const nombre = document.getElementById('nombre').value;
-    const descripcion = document.getElementById('descripcion').value;
-    const tiempo = document.getElementById('tiempo').value;
+    const nombre = $("#nombre").val();
+    const descripcion = $("#descripcion").val();
+    const tiempo = $("#tiempo").val();
+
     const usuario = 'Prof Jimenez';
 
     const nuevoDato = new CursoModel(null, nombre, descripcion, tiempo, usuario);
 
 
-    fetch(`${URLAPI}${crea}`, {
-            method: 'POST',
-            body: JSON.stringify(nuevoDato)
-        })
-        .then(response => response.json())
-        .then(
-            data => finalzarCrear(data)
-        )
-        .catch(error => console.error(error))
+
+    $.ajax({
+        type: "POST",
+        url: `${URLAPI}${crea}`,     
+        data: JSON.stringify(nuevoDato),
+        contentType: 'application/json',
+        dataType: "json",
+        beforeSend: function(){
+            let variableMensaje = `<button
+            class="btn btn-warning"
+            type="button"
+            disabled
+          >
+            <span
+                class="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+            ></span>
+            Loading...
+          </button>`;
+
+          $("#mensajes").html(variableMensaje);
+        },
+        success: function (response) {
+            console.log(response);
+            finalzarCrear(response.data);
+            $("#mensajes").html("");
+        },
+        error: function ( xhr, status, error){
+
+            const mensajeEliminar = `
+                <div
+                            class="alert alert-danger alert-dismissible fade show"
+                            role="alert"
+                        >
+                            <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="alert"
+                                aria-label="Close"
+                            ></button>
+                            <strong>Error: </strong> ${error} <br>
+                            <strong>status: </strong> ${status} <br>
+                            <strong>xhr: </strong> ${xhr} <br>
+                        </div>`;
+                        
+                $("#mensajes").html(mensajeEliminar);
+                
+        }
+        
+        
+    });
+
+    // fetch(`${URLAPI}${crea}`, {
+    //         method: 'POST',
+    //         body: JSON.stringify(nuevoDato)
+    //     })
+    //     .then(response => response.json())
+    //     .then(
+    //         data => finalzarCrear(data)
+    //     )
+    //     .catch(error => console.error(error))
 
 }
 
@@ -167,60 +281,131 @@ function crearCursos() {
 function finalzarCrear(data) {
     obtenerDatos();
 
-    const mensajes = document.getElementById('mensajes');
-    mensajes.innerHTML = mensajeCompletado;
+    // const mensajes = document.getElementById('mensajes');
+    // mensajes.innerHTML = mensajeCompletado;
+    $("#mensajes").html(mensajeCompletado);
 
-    const modalCrear = bootstrap.Modal.getInstance(document.getElementById('modalCrear'));
+    $('#formualCrear')[0].reset();
 
-    modalCrear.hide();
+    // const modalCrear = bootstrap.Modal.getInstance(document.getElementById('modalCrear'));
+    // modalCrear.hide();
+
+    $("#modalCrear").modal('hide');
 }
 
 window.editarCursos = (id, nombre, descripcion, tiempo, usuario) => {
 
-    document.getElementById('ideditar').value = id;
-    document.getElementById('nombreeditar').value = nombre;
-    document.getElementById('descripcioneditar').value = descripcion;
-    document.getElementById('tiempoeditar').value = tiempo;
-    document.getElementById('usuarioeditar').value = usuario;
+    // document.getElementById('ideditar').value = id;
+    // document.getElementById('nombreeditar').value = nombre;
+    // document.getElementById('descripcioneditar').value = descripcion;
+    // document.getElementById('tiempoeditar').value = tiempo;
+    // document.getElementById('usuarioeditar').value = usuario;
 
-    const modalEditar = new bootstrap.Modal(document.getElementById('modalEditar'));
+    $("#ideditar").val(id);
+    $("#nombreeditar").val(nombre);
+    $("#descripcioneditar").val(descripcion);
+    $("#tiempoeditar").val(tiempo);
+    $("#usuarioeditar").val(usuario);
 
-    modalEditar.show();
+    // const modalEditar = new bootstrap.Modal(document.getElementById('modalEditar'));
+
+    // modalEditar.show();
+    $("#modalEditar").modal("show");
 
 }
 
 function actualizarCursos() { 
 
-   const id = document.getElementById('ideditar').value; 
-   const nombre = document.getElementById('nombreeditar').value;
-   const descripcion = document.getElementById('descripcioneditar').value ;
-   const tiempo = document.getElementById('tiempoeditar').value;
-   const usuario = document.getElementById('usuarioeditar').value;
+//    const id = document.getElementById('ideditar').value; 
+//    const nombre = document.getElementById('nombreeditar').value;
+//    const descripcion = document.getElementById('descripcioneditar').value ;
+//    const tiempo = document.getElementById('tiempoeditar').value;
+//    const usuario = document.getElementById('usuarioeditar').value;
+
+        const id = $("#ideditar").val();
+        const nombre =$("#nombreeditar").val();
+        const descripcion =$("#descripcioneditar").val();
+        const tiempo =$("#tiempoeditar").val();
+        const usuario =$("#usuarioeditar").val();
 
    const nuevoDato = new CursoModel(id, nombre, descripcion, tiempo, usuario);
 
+   
+   $.ajax({
+    type: "POST",
+    url: `${URLAPI}${actualiza}`,     
+    data: JSON.stringify(nuevoDato),
+    contentType: 'application/json',
+    dataType: "json",
+    beforeSend: function(){
+        let variableMensaje = `  <button
+        class="btn btn-warning"
+        type="button"
+        disabled
+      >
+        <span
+            class="spinner-border spinner-border-sm"
+            role="status"
+            aria-hidden="true"
+        ></span>
+        Loading...
+      </button>`;
 
-   fetch(`${URLAPI}${actualiza}`, {
-           method: 'POST',
-           body: JSON.stringify(nuevoDato)
-       })
-       .then(response => response.json())
-       .then(
-           data => finalzarActualizar(data)
-       )
-       .catch(error => console.error(error))
- }
+      $("#mensajes").html(variableMensaje);
+    },
+    success: function (response) {
+        console.log(response);
+        finalzarActualizar(response.data);
+        $("#mensajes").html("");
+    },
+    error: function ( xhr, status, error){
+
+        const mensajeEliminar = `
+            <div
+                        class="alert alert-danger alert-dismissible fade show"
+                        role="alert"
+                    >
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="alert"
+                            aria-label="Close"
+                        ></button>
+                        <strong>Error: </strong> ${error} <br>
+                        <strong>status: </strong> ${status} <br>
+                        <strong>xhr: </strong> ${xhr} <br>
+                    </div>`;
+                    
+            $("#mensajes").html(mensajeEliminar);
+            
+    }
+    
+    
+});
+
+//    fetch(`${URLAPI}${actualiza}`, {
+//            method: 'POST',
+//            body: JSON.stringify(nuevoDato)
+//        })
+//        .then(response => response.json())
+//        .then(
+//            data => finalzarActualizar(data)
+//        )
+//        .catch(error => console.error(error))
+}
 
 
  function finalzarActualizar(data){
     obtenerDatos();
 
-    const mensajes = document.getElementById('mensajes');
-    mensajes.innerHTML = mensajeCompletado;
+    // const mensajes = document.getElementById('mensajes');
+    // mensajes.innerHTML = mensajeCompletado;
+    $("#mensajes").html(mensajeCompletado);
 
-    const modalEditar = bootstrap.Modal.getInstance(document.getElementById('modalEditar'));
+    // const modalEditar = bootstrap.Modal.getInstance(document.getElementById('modalEditar'));
+    // modalEditar.hide();
 
-    modalEditar.hide();
+    $("#modalEditar").modal("hide");
  }
 
 
